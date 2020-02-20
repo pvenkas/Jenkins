8fe9cfc93b035b78c1574c8367967a08d660faeb
pvenkas <santosh.prabhu27@gmail.com>
2020-02-19 23:47:25 -0500

    Modified the loop to run 20 times

--- JenkinsInt/src/main/java/Hello.java
+++ JenkinsInt/src/main/java/Hello.java
@@ -1,7 +1,7 @@
 public class Hello {
     public static void main(String[] args) {
 
-        for (int i = 0; i <= 5; i++) {
+        for (int i = 0; i <= 19; i++) {
 
             System.out.println("Hellow World :" + i);
         }
